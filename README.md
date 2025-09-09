403a6b4d020e6229c34508fd2ae5553eb3940eb0
Eudes Nilton <eudes258@gmail.com>
2025-09-09 14:05:51 -0300

    Finalizando "Operadores"

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# Java DevDojo
+## Java DevDojo
 
 # Tipos primitivos
 
 - Criação de nomes de variaveis é estilo camelCase 
 - Casting -> int number = (int) 100000000; //NÃO É UMA BOA PRATICA 
 - a String não é um tipo primitivo e sim uma Classe
+
+# Operadores
+
+- O resultado de soma, multiplicação, divisão e adição de inteiros sempre sera um número inteiro
+- Os valores lógicos sempre vão resultar em um valor booleano (true or false)
+- Operadores aritméticos +, -, *, /
+- Operador de resto %
+- Operador de comparação <, >, <=, >=, ==, !=
+- Operadores lógicos são &&(and), ||(or), !(not)
+- Operadores de atribuição =, +=, -=, *=, /=, %=
---
+++ src/Aula04Operadores.java
@@ -0,0 +1,67 @@
+public class Aula04Operadores {
+
+    public static void main(String[] args) {
+        // Operadores Aritméticos
+        // +, -, *, /
+
+        int a = 10;
+        int b = 5;
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Subtração: " + (a - b));
+        System.out.println("Multiplicação: " + (a * b));
+        System.out.println("Divisão: " + (a / b));
+    
+        // operador de resto %
+        
+        System.out.println("Resto da Divisão: " + (a % b));
+
+        // Operadores de comparação
+        // <, >, <=, >=, ==, !=
+
+        boolean isEqual = (a == b);
+        boolean isNotEqual = (a != b);
+        boolean isGreater = (a > b);
+        boolean isLess = (a < b);
+        boolean isGreaterOrEqual = (a >= b);
+        boolean isLessOrEqual = (a <= b);
+
+        System.out.println("isEqual: " + isEqual);
+        System.out.println("isNotEqual: " + isNotEqual);
+        System.out.println("isGreater: " + isGreater);
+        System.out.println("isLess: " + isLess);
+        System.out.println("isGreaterOrEqual: " + isGreaterOrEqual);
+        System.out.println("isLessOrEqual: " + isLessOrEqual);
+
+        // Operadores Lógicos
+        // && (AND), || (OR), ! (NOT)
+
+        boolean andResult = (a > b) && (a != b); //precisa que todos sejam verdadeiros
+        boolean orResult = (a < b) || (a != b); //precisa que pelo menos um seja verdadeiro
+        boolean notResult = !(a != b); //inverte o resultado
+
+        System.out.println("AND Result: " + andResult);
+        System.out.println("OR Result: " + orResult);
+        System.out.println("NOT Result: " + notResult);
+
+        // Operadores de Atribuição
+        // =, +=, -=, *=, /=, %=
+        
+        int c = 20;
+        c += 10; // c = c + 10
+        System.out.println("c after += 10: " + c);
+
+        c -= 5; // c = c - 5
+        System.out.println("c after -= 5: " + c);
+
+        c *= 2; // c = c * 2
+        System.out.println("c after *= 2: " + c);
+
+        c /= 5; // c = c / 5
+        System.out.println("c after /= 5: " + c);
+
+        c %= 3; // c = c % 3
+        System.out.println("c after %= 3: " + c);
+
+
+    }
+}
