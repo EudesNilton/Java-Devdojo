7866ab16de29bff2a5f511fcd984b4eac85b89a9
Eudes Nilton <eudes258@gmail.com>
2025-09-09 11:28:39 -0300

    Finalizado "Tipos primitivos"

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 # Tipos primitivos
 
 - Criação de nomes de variaveis é estilo camelCase 
-- 
+- Casting -> int number = (int) 100000000; //NÃO É UMA BOA PRATICA 
+- a String não é um tipo primitivo e sim uma Classe
---
+++ src/aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+public class aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // Tipos primitivos em Java
+        // int, double, float, char, boolean
+        int intNumber = 10; // Tipo inteiro
+        double doubleNumber = 10.5; // Tipo decimal
+        float floatNumber = 5.5F; // Tipo float
+        char charValue = 'A'; // Tipo caractere
+        boolean isBooleanValue = true; // Tipo booleano
+        String stringValue = "Olá, Mundo!"; // Tipo String (não é primitivo, mas muito usado)
+        
+        // Exibindo os valores
+        System.out.println("Número Inteiro: " + intNumber);
+        System.out.println("Número Decimal: " + doubleNumber);
+        System.out.println("Número Float: " + floatNumber);
+        System.out.println("Caractere: " + charValue);
+        System.out.println("Booleano: " + isBooleanValue);
+        System.out.println("String: " + stringValue);
+    }
+}
