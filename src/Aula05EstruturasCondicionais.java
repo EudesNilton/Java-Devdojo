9f31b379acba41ddc96cfd2d1ab5c086b7230821
Eudes Nilton <eudes258@gmail.com>
2025-09-15 13:28:06 -0300

    Finalizado "Condicionais"

--- src/Aula05EstruturasCondicionais.java
+++ src/Aula05EstruturasCondicionais.java
@@ -40,5 +40,38 @@ public class Aula05EstruturasCondicionais {
         // condição ? verdadeiro : falso
         String result = (salary > 5000) ? messageDonate : messageNotDonate;
         System.out.println(result);
+
+
+        // switch case
+        int number = 4;
+        String day;
+
+        switch(number){
+            case 1:
+                day = "Domingo";
+                break; // para o código aqui
+            case 2:
+                day = "Segunda";
+                break; // para o código aqui
+            case 3:
+                day = "Terça";
+                break; // para o código aqui 
+            case 4:
+                day = "Quarta";
+                break; // para o código aqui
+            case 5:
+                day = "Quinta";
+                break; // para o código aqui
+            case 6:
+                day = "Sexta";
+                break; // para o código aqui
+            case 7:
+                day = "Sábado";
+                break; // para o código aqui
+            default:
+                day = "Dia inválido";
+                break; // para o código aqui
+        }
+        System.out.println(day);
     }
 }
