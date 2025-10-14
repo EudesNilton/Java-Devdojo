662d1f20e007444b8c139a5353393ad3f8d85676
Eudes Nilton <eudes258@gmail.com>
2025-10-14 12:00:01 -0300

    OO - Associação  - Bidirecional

--- src/javacore/associacao/dominio/Time.java
+++ src/javacore/associacao/dominio/Time.java
@@ -2,9 +2,25 @@ package javacore.associacao.dominio;
 
 public class Time {
     private String name;
+    private Jogador[] jogadores;
+
+    public Time(String name, Jogador[] jogadores) {
+        this.name = name;
+        this.jogadores = jogadores;
+    }
 
     public Time(String name) {
         this.name = name;
+    }
+
+    public void imprime(){
+        System.out.println(this.name);
+        if (jogadores == null) {
+            return;
+        }
+        for (Jogador jogador : jogadores) {
+            System.out.println(jogador.getName());
+        }
     }
     
     public String getName() {
@@ -13,6 +29,14 @@ public class Time {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
     }
 
     
