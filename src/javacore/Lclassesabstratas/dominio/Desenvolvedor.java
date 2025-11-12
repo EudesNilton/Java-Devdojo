da479f8beb088ed74a926080f3e0c1953fbe671f
Eudes Nilton <eudes258@gmail.com>
2025-11-12 13:20:18 -0300

    OO - Classes Abstratas - Métodos Abstratos regras

--- src/javacore/Lclassesabstratas/dominio/Desenvolvedor.java
+++ src/javacore/Lclassesabstratas/dominio/Desenvolvedor.java
@@ -4,12 +4,10 @@ public class Desenvolvedor extends Funcionario {
 
     public Desenvolvedor(String nome, double salario) {
         super(nome, salario);
-        //TODO Auto-generated constructor stub
     }
 
     @Override
     public void calculaBonus() {
-        // TODO Auto-generated method stub
         this.salario = this.salario + this.salario * 0.05;
     }
 
--- src/javacore/Lclassesabstratas/dominio/Gerente.java
+++ src/javacore/Lclassesabstratas/dominio/Gerente.java
@@ -4,12 +4,10 @@ public class Gerente extends Funcionario{
 
     public Gerente(String nome, double salario) {
         super(nome, salario);
-        //TODO Auto-generated constructor stub
     }   
 
     @Override
     public void calculaBonus() {
-        // TODO Auto-generated method stub
         this.salario = this.salario + this.salario * 0.2;
     }
     
