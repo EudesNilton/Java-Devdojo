05478147a5465eee88a171e5de9ff9c4447b24aa
Eudes Nilton <eudes258@gmail.com>
2025-11-12 11:48:07 -0300

    OO - Classes Abstratas - Introdução

---
+++ src/javacore/Lclassesabstratas/dominio/Gerente.java
@@ -0,0 +1,15 @@
+package javacore.Lclassesabstratas.dominio;
+
+public class Gerente extends Funcionario{
+
+    public Gerente(String nome, double salario) {
+        super(nome, salario);
+        //TODO Auto-generated constructor stub
+    }   
+    
+    @Override
+    public String toString() {
+        return "Gerente [nome=" + nome + ", salario=" + salario + "]";
+    }
+    
+}
